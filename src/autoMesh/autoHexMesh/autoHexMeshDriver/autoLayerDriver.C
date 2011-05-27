e0f1fc33093295c2a827a6ad1a8d2d67afc1bac0
Hrvoje Jasak <h.jasak@wikki.co.uk>
2011-05-27 11:39:16 +0100

    fvMesh to polyMesh clean-up for directTopoChange

--- src/autoMesh/autoHexMesh/autoHexMeshDriver/autoLayerDriver.C
+++ src/autoMesh/autoHexMesh/autoHexMeshDriver/autoLayerDriver.C
@@ -2332,7 +2332,7 @@ Foam::label Foam::autoLayerDriver::checkAndUnmark
     const addPatchCellLayer& addLayer,
     const dictionary& meshQualityDict,
     const indirectPrimitivePatch& pp,
-    const fvMesh& newMesh,
+    const polyMesh& newMesh,
 
     pointField& patchDisp,
     labelList& patchNLayers,
@@ -3055,7 +3055,7 @@ void Foam::autoLayerDriver::addLayers
         // With the stored topo changes we create a new mesh so we can
         // undo if neccesary.
 
-        autoPtr<fvMesh> newMeshPtr;
+        autoPtr<polyMesh> newMeshPtr;
         autoPtr<mapPolyMesh> map = meshMod.makeMesh
         (
             newMeshPtr,
@@ -3071,10 +3071,9 @@ void Foam::autoLayerDriver::addLayers
             mesh,           // original mesh
             true            // parallel sync
         );
-        fvMesh& newMesh = newMeshPtr();
+        polyMesh& newMesh = newMeshPtr();
 
-        //?neccesary? Update fields
-        newMesh.updateMesh(map);
+        // Update fields: removed.  HJ, 19/May/2011
 
         if (meshRefiner_.overwrite())
         {
@@ -3105,6 +3104,7 @@ void Foam::autoLayerDriver::addLayers
         {
             Info<< "Writing layer mesh to " << meshRefiner_.timeName() << endl;
             newMesh.write();
+
             cellSet addedCellSet
             (
                 newMesh,
