1c715bbc963cab38065f5c107e3db85442e56751
Martin Beaudoin <beaudoin.martin@ireq.ca>
2011-06-04 18:55:09 -0400

    Adjustments for cmake

--- etc/settings.sh
+++ etc/settings.sh
@@ -480,8 +480,11 @@ export MPI_BUFFER_SIZE
 
 # Load cmake
 # ~~~~~~~~~~
-[ -z "$CMAKE_SYSTEM" ] && [ -e $WM_THIRD_PARTY_DIR/packages/cmake-2.8.3 ] && {
-    _foamSource $WM_THIRD_PARTY_DIR/packages/cmake-2.8.3/platforms/$WM_OPTIONS/etc/cmake-2.8.3.sh
+#[ -z "$CMAKE_SYSTEM" ] && [ -e $WM_THIRD_PARTY_DIR/packages/cmake-2.8.3 ] && {
+#    _foamSource $WM_THIRD_PARTY_DIR/packages/cmake-2.8.3/platforms/$WM_OPTIONS/etc/cmake-2.8.3.sh
+#}
+[ -z "$CMAKE_SYSTEM" ] && [ -e $WM_THIRD_PARTY_DIR/packages/cmake-2.8.git ] && {
+    _foamSource $WM_THIRD_PARTY_DIR/packages/cmake-2.8.git/platforms/$WM_OPTIONS/etc/cmake-2.8.git.sh
 }
 [ "$FOAM_VERBOSE" -a "$PS1" ] && echo "    CMAKE_DIR is initialized to: $CMAKE_DIR"
 
