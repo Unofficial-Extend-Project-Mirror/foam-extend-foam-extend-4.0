0b8e7cdd49535b8922aa2e23af2e46b85b5c1fd1
Hrvoje Jasak <h.jasak@wikki.co.uk>
2011-10-06 15:04:49 +0100

    Formatting

--- src/OpenFOAM/meshes/primitiveMesh/primitiveMeshCheck/primitiveMeshCheckMotion.C
+++ src/OpenFOAM/meshes/primitiveMesh/primitiveMeshCheck/primitiveMeshCheckMotion.C
@@ -23,8 +23,8 @@ License
     Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 Description
-    Given a set of points, find out if the mesh resulting from point motion will
-    be valid without actually changing the mesh.
+    Given a set of points, find out if the mesh resulting from point motion
+    will be valid without actually changing the mesh.
 
 \*---------------------------------------------------------------------------*/
 
@@ -32,9 +32,6 @@ Description
 #include "pyramidPointFaceRef.H"
 #include "cell.H"
 #include "mathematicalConstants.H"
-
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-
 
 // * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
 
