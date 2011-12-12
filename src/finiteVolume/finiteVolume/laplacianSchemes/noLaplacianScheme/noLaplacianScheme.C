cf4daeb57fa3a7b2db9e37202cfdcee091ec5322
Hrvoje Jasak <h.jasak@wikki.co.uk>
2011-12-12 16:23:55 +0000

    Formatting

--- src/finiteVolume/finiteVolume/laplacianSchemes/noLaplacianScheme/noLaplacianScheme.C
+++ src/finiteVolume/finiteVolume/laplacianSchemes/noLaplacianScheme/noLaplacianScheme.C
@@ -21,7 +21,7 @@ License
     You should have received a copy of the GNU General Public License
     along with OpenFOAM; if not, write to the Free Software Foundation,
     Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-    
+
 \*---------------------------------------------------------------------------*/
 
 #include "noLaplacianScheme.H"
@@ -94,6 +94,9 @@ noLaplacianScheme<Type, GType>::fvmLaplacian
             magSf.dimensions()*vf.dimensions()
         )
     );
+
+    // Create dummy diagonal
+    tfvm().diag() = 0;
 
     return tfvm;
 }
