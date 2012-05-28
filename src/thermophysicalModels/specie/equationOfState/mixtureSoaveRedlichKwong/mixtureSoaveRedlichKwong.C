f3aad569987117fde0f634ccfb61db6e1305d2bd
Christian Lucas <ch.lucas@tu-bs.de>
2012-05-28 21:00:14 +0200

    mixture version of real gas EOS

---
+++ src/thermophysicalModels/specie/equationOfState/mixtureSoaveRedlichKwong/mixtureSoaveRedlichKwong.C
@@ -0,0 +1,73 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright held by original author
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Description
+    Soave Redlich Kwong equation of state for mixtures.
+
+Author
+Christian Lucas
+Institut für Thermodynamik
+Technische Universität Braunschweig 
+Germany
+
+\*---------------------------------------------------------------------------*/
+
+#include "mixtureSoaveRedlichKwong.H"
+#include "IOstreams.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+mixtureSoaveRedlichKwong::mixtureSoaveRedlichKwong(Istream& is)
+:
+    soaveRedlichKwong(is),
+    numOfComp(1),
+    singleComponent(1)
+{ 
+    //CL: Save a pointer of this object in the mixtureComponents array
+    mixtureComponents.push_back(this);
+    is.check("mixtureSoaveRedlichKwong::mixtureSoaveRedlichKwong(Istream& is)");
+}
+
+
+// * * * * * * * * * * * * * * * Ostream Operator  * * * * * * * * * * * * * //
+
+Ostream& operator<<(Ostream& os, const mixtureSoaveRedlichKwong& srk)
+{
+    os  << static_cast<const soaveRedlichKwong&>(srk)<< tab;
+
+    os.check("Ostream& operator<<(Ostream& os, const mixtureSoaveRedlichKwong& st)");
+    return os;
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
