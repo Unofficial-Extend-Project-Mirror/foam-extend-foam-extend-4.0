fbf51d5ededbc716f3690a8091302c61462afa80
Bernhard Gschaider <bgschaid@ice-sf.at>
2014-05-16 14:06:25 +0200

    Bootstrap-script adds a log file to home

--- vagrantSandbox/skel/bootstrapFoam.sh
+++ vagrantSandbox/skel/bootstrapFoam.sh
@@ -4,19 +4,22 @@
 export WM_SCHEDULER=ccache
 export CCACHE_DIR=/vagrant/ccache4vm
 
+BOOTSTRAPLOG=/home/vagrant/bootstrapFoam.log
+
 cd foam/foam-extend-3.0
 source etc/bashrc
 
 ( cd wmake/src && make )
 cd $WM_THIRD_PARTY_DIR
 
-./AllMake.stage0
-./AllMake.stage1
-./AllMake.stage2
-./AllMake.stage3
+./AllMake.stage0  2>&1 | tee $BOOTSTRAPLOG
+./AllMake.stage1  2>&1 | tee --append $BOOTSTRAPLOG
+./AllMake.stage2  2>&1 | tee --append $BOOTSTRAPLOG
+./AllMake.stage3  2>&1 | tee --append $BOOTSTRAPLOG
 
 cd $WM_PROJECT_DIR
 # pick up installed packages
 source etc/bashrc
 
-./Allwmake
+./Allwmake  2>&1 | tee --append $BOOTSTRAPLOG
+
