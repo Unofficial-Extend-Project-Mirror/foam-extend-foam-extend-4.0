d7e5fd09b73fe1ccec60e9bccdc16b5f39099a8e
Henrik Rusche <h.rusche@wikki-gmbh.de>
2015-08-11 12:19:32 +0200

    Upgrade copyright notices & banner in source code

--- src/OSspecific/MSWindows/signals/sigFpe.C
+++ src/OSspecific/MSWindows/signals/sigFpe.C
@@ -1,26 +1,34 @@
 /*---------------------------------------------------------------------------*\
-    Copyright            : (C) 2011 Symscape
-    Website              : www.symscape.com
+  =========                 |
+  \\      /  F ield         | foam-extend: Open Source CFD
+   \\    /   O peration     | Version:     3.2
+    \\  /    A nd           | Web:         http://www.foam-extend.org
+     \\/     M anipulation  | For copyright notice see file Copyright
 -------------------------------------------------------------------------------
 License
-    This file is part of OpenFOAM.
+    This file is part of foam-extend.
 
-    OpenFOAM is free software: you can redistribute it and/or modify it
-    under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    foam-extend is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
 
-    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
-    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-    for more details.
+    foam-extend is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+    along with foam-extend.  If not, see <http://www.gnu.org/licenses/>.
 
 Class
     sigFpe
 
+\*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*\
+    Copyright            : (C) 2011 Symscape
+    Website              : www.symscape.com
 \*---------------------------------------------------------------------------*/
 
 #include "error.H"
