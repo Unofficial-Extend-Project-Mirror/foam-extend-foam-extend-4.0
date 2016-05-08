f609442e998d5da125fda0648630d922ed28ee59
Vanja Skuric <vanja.skuric@fsb.hr>
2016-05-08 11:28:13 +0200

    Backported dieselFoam, dieselEngineFoam, fireFoam, reactingFoam and tutorials (vanilla OF 3.0.1)

--- applications/solvers/combustion/dieselEngineFoam/dieselEngineFoam.C
+++ applications/solvers/combustion/dieselEngineFoam/dieselEngineFoam.C
@@ -42,6 +42,7 @@ Description
 #include "OFstream.H"
 #include "volPointInterpolation.H"
 #include "thermoPhysicsTypes.H"
+#include "pimpleControl.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
@@ -50,6 +51,9 @@ int main(int argc, char *argv[])
 #   include "setRootCase.H"
 #   include "createEngineTime.H"
 #   include "createEngineMesh.H"
+
+    pimpleControl pimple(mesh);
+
 #   include "createFields.H"
 #   include "readGravitationalAcceleration.H"
 #   include "readCombustionProperties.H"
@@ -67,7 +71,6 @@ int main(int argc, char *argv[])
 
     while (runTime.run())
     {
-#       include "readPISOControls.H"
 #       include "readEngineTimeControls.H"
 #       include "compressibleCourantNo.H"
 #       include "setDeltaT.H"
@@ -103,13 +106,13 @@ int main(int argc, char *argv[])
 #       include "rhoEqn.H"
 #       include "UEqn.H"
 
-        for (label ocorr=1; ocorr <= nOuterCorr; ocorr++)
+        while (pimple.loop())
         {
 #           include "YEqn.H"
 #           include "hsEqn.H"
 
             // --- PISO loop
-            for (int corr=1; corr<=nCorr; corr++)
+            while (pimple.correct())
             {
 #               include "pEqn.H"
             }
