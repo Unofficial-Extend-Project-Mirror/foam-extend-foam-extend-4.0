07e9416f30a23bd723789043417a3e80b083374c
Henrik Rusche <h.rusche@wikki-gmbh.de>
2016-06-20 17:00:40 +0200

    Bump version in headers

--- src/foam/primitives/ints/long/long.H
+++ src/foam/primitives/ints/long/long.H
@@ -1,7 +1,7 @@
 /*---------------------------------------------------------------------------*\
   =========                 |
   \\      /  F ield         | foam-extend: Open Source CFD
-   \\    /   O peration     | Version:     3.2
+   \\    /   O peration     | Version:     4.0
     \\  /    A nd           | Web:         http://www.foam-extend.org
      \\/     M anipulation  | For copyright notice see file Copyright
 -------------------------------------------------------------------------------
