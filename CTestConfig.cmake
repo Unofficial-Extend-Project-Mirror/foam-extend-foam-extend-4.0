09baafc79a98d98776d5e2c231ba96d5c9aac2f4
Hrvoje Jasak <h.jasak@wikki.co.uk>
2016-08-01 16:45:37 +0100

    Bugfix: Corrected CDDash repo to 4.0.  Martin Beaudoin

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -15,7 +15,7 @@ set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "foam-extend.sourceforge.net")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=foam-extend-3.2")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=foam-extend-4.0")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
 # We can override those variables for local sites so you can use
