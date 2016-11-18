8e436f6616b35e3e306b31bb81053969eefbaecc
Henrik Rusche <h.rusche@wikki-gmbh.de>
2016-11-18 14:04:12 +0100

    Updated download location; bumped paraview version

--- etc/settings.sh
+++ etc/settings.sh
@@ -740,6 +740,9 @@ fi
 
 # Load ParaView
 # ~~~~~~~~~~~~~
+[ -z "$PARAVIEW_SYSTEM" ] && [ ! -z $WM_THIRD_PARTY_USE_PARAVIEW_440 ] && [ -e $WM_THIRD_PARTY_DIR/packages/ParaView-4.4.0/platforms/$WM_OPTIONS ] && {
+    _foamSource $WM_THIRD_PARTY_DIR/packages/ParaView-4.4.0/platforms/$WM_OPTIONS/etc/ParaView-4.4.0.sh
+}
 [ -z "$PARAVIEW_SYSTEM" ] && [ ! -z $WM_THIRD_PARTY_USE_PARAVIEW_431 ] && [ -e $WM_THIRD_PARTY_DIR/packages/ParaView-4.3.1/platforms/$WM_OPTIONS ] && {
     _foamSource $WM_THIRD_PARTY_DIR/packages/ParaView-4.3.1/platforms/$WM_OPTIONS/etc/ParaView-4.3.1.sh
 }
