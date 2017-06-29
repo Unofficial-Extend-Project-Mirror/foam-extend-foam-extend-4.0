ec8b2eb97b2f4c2ed748a2ac8dbe5e105cdae6a1
Vuko Vukcevic <v.vukcevic@wikki.co.uk>
2017-06-29 08:56:04 +0200

    nuRatio boundary conditions for epsilon and omega
    Given a desired nuRatio, the boundary conditions automatically calculate
    epsilon/omega from k.

---
+++ src/turbulenceModels/incompressible/RAS/derivedFvPatchFields/nuRatioEpsilon/nuRatioEpsilonFvPatchScalarField.C
@@ -0,0 +1,205 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | foam-extend: Open Source CFD
+   \\    /   O peration     | Version:     4.0
+    \\  /    A nd           | Web:         http://www.foam-extend.org
+     \\/     M anipulation  | For copyright notice see file Copyright
+-------------------------------------------------------------------------------
+License
+    This file is part of foam-extend.
+
+    foam-extend is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    foam-extend is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with foam-extend.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "nuRatioEpsilonFvPatchScalarField.H"
+#include "addToRunTimeSelectionTable.H"
+#include "RASModel.H"
+#include "fvPatchFieldMapper.H"
+#include "surfaceFields.H"
+#include "volFields.H"
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::nuRatioEpsilonFvPatchScalarField::nuRatioEpsilonFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    inletOutletFvPatchScalarField(p, iF),
+    nuRatio_(1),
+    Cmu_(0.09),
+    kName_("undefined-k"),
+    phiName_("undefined-phi")
+{
+    this->refValue() = 0.0;
+    this->refGrad() = 0.0;
+    this->valueFraction() = 0.0;
+}
+
+
+Foam::nuRatioEpsilonFvPatchScalarField::nuRatioEpsilonFvPatchScalarField
+(
+    const nuRatioEpsilonFvPatchScalarField& ptf,
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const fvPatchFieldMapper& mapper
+)
+:
+    inletOutletFvPatchScalarField(ptf, p, iF, mapper),
+    nuRatio_(ptf.nuRatio_),
+    Cmu_(ptf.Cmu_),
+    kName_(ptf.kName_),
+    phiName_(ptf.phiName_)
+{}
+
+
+Foam::nuRatioEpsilonFvPatchScalarField::nuRatioEpsilonFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const dictionary& dict
+)
+:
+    inletOutletFvPatchScalarField(p, iF),
+    nuRatio_(readScalar(dict.lookup("nuRatio"))),
+    Cmu_(dict.lookupOrDefault<scalar>("Cmu", 0.09)),
+    kName_(dict.lookupOrDefault<word>("kName", "k")),
+    phiName_(dict.lookupOrDefault<word>("phiName", "phi"))
+{
+    if (nuRatio_< SMALL)
+    {
+        FatalErrorIn
+        (
+            "nuRatioEpsilonFvPatchScalarField::nuRatioEpsilonFvPatchScalarField"
+            "(const fvPatch& p, const DimensionedField<scalar, volMesh>& iF, "
+            "const dictionary& dict)"
+        )   << "Invalid eddy viscosity ratio (nuRatio) specified: " << nuRatio_
+            << "\n    on patch " << this->patch().name()
+            << " of field " << this->dimensionedInternalField().name()
+            << " in file " << this->dimensionedInternalField().objectPath()
+            << exit(FatalError);
+    }
+
+    if (Cmu_ < SMALL)
+    {
+        FatalErrorIn
+        (
+            "nuRatioEpsilonFvPatchScalarField::nuRatioEpsilonFvPatchScalarField"
+            "(const fvPatch& p, const DimensionedField<scalar, volMesh>& iF, "
+            "const dictionary& dict)"
+        )   << "Invalid Cmu_ constant specified: " << Cmu_
+            << "\n    on patch " << this->patch().name()
+            << " of field " << this->dimensionedInternalField().name()
+            << " in file " << this->dimensionedInternalField().objectPath()
+            << exit(FatalError);
+    }
+
+    fvPatchScalarField::operator=(scalarField("value", dict, p.size()));
+
+    this->refValue() = 0.0;
+    this->refGrad() = 0.0;
+    this->valueFraction() = 0.0;
+}
+
+
+Foam::nuRatioEpsilonFvPatchScalarField::nuRatioEpsilonFvPatchScalarField
+(
+    const nuRatioEpsilonFvPatchScalarField& ptf
+)
+:
+    inletOutletFvPatchScalarField(ptf),
+    nuRatio_(ptf.nuRatio_),
+    Cmu_(ptf.Cmu_),
+    kName_(ptf.kName_),
+    phiName_(ptf.phiName_)
+{}
+
+
+Foam::nuRatioEpsilonFvPatchScalarField::nuRatioEpsilonFvPatchScalarField
+(
+    const nuRatioEpsilonFvPatchScalarField& ptf,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    inletOutletFvPatchScalarField(ptf, iF),
+    nuRatio_(ptf.nuRatio_),
+    Cmu_(ptf.Cmu_),
+    kName_(ptf.kName_),
+    phiName_(ptf.phiName_)
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void Foam::nuRatioEpsilonFvPatchScalarField::updateCoeffs()
+{
+    if (updated())
+    {
+        return;
+    }
+
+    // Get turbulent kinetic energy field for this patch
+    const fvPatchScalarField& kp =
+        lookupPatchField<volScalarField, vector>(kName_);
+
+    // Get flux field for this patch
+    const fvsPatchScalarField& phip =
+        lookupPatchField<surfaceScalarField, scalar>(phiName_);
+
+    // Get RASModel
+    const incompressible::RASModel& rasModel =
+        this->dimensionedInternalField().mesh().lookupObject
+        <
+            incompressible::RASModel
+        >("RASProperties");
+
+    // Get laminar viscosity for this patch
+    const fvPatchScalarField& nup =
+        rasModel.nu().boundaryField()[this->patch().index()];
+
+    this->refValue() = Cmu_*sqrt(kp)/nuRatio_/nup;
+    this->valueFraction() = neg(phip);
+
+    inletOutletFvPatchScalarField::updateCoeffs();
+}
+
+
+void Foam::nuRatioEpsilonFvPatchScalarField::write
+(
+    Ostream& os
+) const
+{
+    fvPatchScalarField::write(os);
+    os.writeKeyword("nuRatio") << nuRatio_ << token::END_STATEMENT << nl;
+    os.writeKeyword("Cmu") << Cmu_ << token::END_STATEMENT << nl;
+    os.writeKeyword("kName") << kName_ << token::END_STATEMENT << nl;
+    os.writeKeyword("phiName") << phiName_ << token::END_STATEMENT << nl;
+    writeEntry("value", os);
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    makePatchTypeField
+    (
+        fvPatchScalarField,
+        nuRatioEpsilonFvPatchScalarField
+    );
+}
+
+// ************************************************************************* //
