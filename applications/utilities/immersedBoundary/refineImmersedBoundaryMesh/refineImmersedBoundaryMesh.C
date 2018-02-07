f987749bdea02053ce47cf1949fb5116b564840f
Hrvoje Jasak <h.jasak@wikki.co.uk>
2018-02-07 14:45:34 +0000

    Formatting

--- applications/utilities/immersedBoundary/refineImmersedBoundaryMesh/refineImmersedBoundaryMesh.C
+++ applications/utilities/immersedBoundary/refineImmersedBoundaryMesh/refineImmersedBoundaryMesh.C
@@ -38,7 +38,6 @@ int main(int argc, char *argv[])
 {
     argList::validOptions.insert("ibCells", "");
     argList::validOptions.insert("ibCellCells", "");
-    argList::validOptions.insert("ibCellCellFaces", "");
 
 #   include "setRootCase.H"
 
