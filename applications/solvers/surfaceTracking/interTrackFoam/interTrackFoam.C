11040181335e1bb241e36bf2736794dde67ef82d
Hrvoje Jasak <h.jasak@wikki.co.uk>
2018-05-16 12:36:55 +0100

    Fixed runTime control bug

--- applications/solvers/surfaceTracking/interTrackFoam/interTrackFoam.C
+++ applications/solvers/surfaceTracking/interTrackFoam/interTrackFoam.C
@@ -62,6 +62,10 @@ int main(int argc, char *argv[])
 #       include "CourantNo.H"
         //#       include "setSurfaceStabilityDeltaT.H"
 
+        runTime++;
+
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
         interface.moveMeshPointsForOldFreeSurfDisplacement();
 
         interface.updateDisplacementDirections();
