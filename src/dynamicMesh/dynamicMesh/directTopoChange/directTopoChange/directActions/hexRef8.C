23ef4e1dc3d738e15244c06adc9e5d192283db23
Danial Khazaei <khazaeidanial@gmail.com>
2019-01-31 17:11:22 +0330

    Bugfix: overflow in conversion from Foam::label to int

--- src/dynamicMesh/dynamicMesh/directTopoChange/directTopoChange/directActions/hexRef8.C
+++ src/dynamicMesh/dynamicMesh/directTopoChange/directTopoChange/directActions/hexRef8.C
@@ -50,7 +50,7 @@ namespace Foam
     defineTypeNameAndDebug(hexRef8, 0);
 
     //- Reduction class. If x and y are not equal assign value.
-    template< int value >
+    template<label value>
     class ifEqEqOp
     {
         public:
