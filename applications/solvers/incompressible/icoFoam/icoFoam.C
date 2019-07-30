3164a804597a9b0007f743a41567518621ae5478
Hrvoje Jasak <h.jasak@wikki.co.uk>
2019-07-30 11:44:34 +0100

    Do not use UFinal in icoFoam

--- applications/solvers/incompressible/icoFoam/icoFoam.C
+++ applications/solvers/incompressible/icoFoam/icoFoam.C
@@ -105,10 +105,7 @@ int main(int argc, char *argv[])
                 );
 
                 pEqn.setReference(pRefCell, pRefValue);
-                pEqn.solve
-                (
-                    mesh.solutionDict().solver(p.select(piso.finalInnerIter()))
-                );
+                pEqn.solve();
 
                 if (piso.finalNonOrthogonalIter())
                 {
