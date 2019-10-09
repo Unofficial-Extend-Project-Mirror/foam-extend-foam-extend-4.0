e11ea2f9397aa81b1b72d91d6e52d914db7d5dea
Hrvoje Jasak <h.jasak@wikki.co.uk>
2019-10-09 12:48:13 +0100

    Wall heat flux utility for sonicFoam/sonicDyMFoam

---
+++ applications/utilities/postProcessing/wall/sonicWallHeatFlux/sonicWallHeatFlux.C
@@ -0,0 +1,104 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | foam-extend: Open Source CFD
+   \\    /   O peration     | Version:     4.1
+    \\  /    A nd           | Web:         http://www.foam-extend.org
+     \\/     M anipulation  | For copyright notice see file Copyright
+-------------------------------------------------------------------------------
+License
+    This file is part of foam-extend.
+
+    foam-extend is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    foam-extend is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with foam-extend.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    wallHeatFlux
+
+Description
+    Calculates and writes the heat flux for all patches as the boundary field
+    of a volScalarField and also prints the integrated flux for all wall
+    patches.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "basicPsiThermo.H"
+#include "RASModel.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    timeSelector::addOptions();
+#   include "setRootCase.H"
+#   include "createTime.H"
+    instantList timeDirs = timeSelector::select0(runTime, args);
+#   include "createMesh.H"
+
+    forAll(timeDirs, timeI)
+    {
+        runTime.setTime(timeDirs[timeI], timeI);
+        Info<< "Time = " << runTime.timeName() << endl;
+        mesh.readUpdate();
+
+#       include "createFields.H"
+
+        surfaceScalarField heatFlux =
+            fvc::interpolate(RASModel->alphaEff())*fvc::snGrad(e);
+
+        const surfaceScalarField::GeometricBoundaryField& patchHeatFlux =
+            heatFlux.boundaryField();
+
+        Info<< "\nWall heat fluxes [W]" << endl;
+        forAll(patchHeatFlux, patchi)
+        {
+            if (mesh.boundary()[patchi].isWall())
+            {
+                Info<< mesh.boundary()[patchi].name()
+                    << " "
+                    << sum
+                       (
+                           mesh.magSf().boundaryField()[patchi]
+                          *patchHeatFlux[patchi]
+                       )
+                    << endl;
+            }
+        }
+        Info<< endl;
+
+        volScalarField wallHeatFlux
+        (
+            IOobject
+            (
+                "wallHeatFlux",
+                runTime.timeName(),
+                mesh
+            ),
+            mesh,
+            dimensionedScalar("wallHeatFlux", heatFlux.dimensions(), 0.0)
+        );
+
+        forAll(wallHeatFlux.boundaryField(), patchi)
+        {
+            wallHeatFlux.boundaryField()[patchi] = patchHeatFlux[patchi];
+        }
+
+        wallHeatFlux.write();
+    }
+
+    Info<< "End" << endl;
+
+    return 0;
+}
+
+// ************************************************************************* //
